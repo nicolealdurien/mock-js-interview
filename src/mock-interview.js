84e2a2e4b9df359d043a5e6e638f1c1e206a5932
codeemi1y <codeemily4@gmail.com>
2023-11-19 22:07:18 -0500

    Initial state of mock interview

---
+++ src/mock-interview.js
@@ -0,0 +1,40 @@
+const dogs = [
+    { name: 'Alfred', age: 7, breed: 'Pug', breedCode: 'pug' },
+    { name: 'Zeke', age: 7, breed: 'Boxer', breedCode: 'boxer' },
+    { name: 'Fish', age: 2, breed: 'Australian Shepherd', breedCode: 'australian/shepherd' },
+    { name: 'Max', age: 2, breed: 'Boxer', breedCode: 'boxer' },
+    { name: 'Wiley', age: 3, breed: 'Cardigan Corgi', breedCode: 'corgi/cardigan' },
+    { name: 'Mickey', age: 4, breed: 'Cardigan Corgi', breedCode: 'corgi/cardigan' },
+    { name: 'Squirrel', age: 1, breed: 'Boxer', breedCode: 'boxer' },
+    { name: 'Beatrice', age: 5, breed: 'Pug', breedCode: 'pug' },
+    { name: 'Eevee', age: 5, breed: 'Pembroke Welsh Corgi', breedCode: 'pembroke' },
+];
+
+// Instructions:
+
+// Using the array above as an input, write a function returning an array of dogs 3 and older
+
+
+// Using the array above as an input, write a function returning an array of all the dog names
+
+
+// Using the array above as an input, write a function returning the youngest dog
+
+
+// Using the array above as an input, write a function returning how many times each dog breed shows up in the array
+
+
+
+/* in the App.jsx
+ This repository is a React app spun up using Vite. You'll see an App.jsx file you can edit and make your own.
+ Feel free to add any React components in the assets/components folder
+
+ Display a random image for each breed that appears in the above array. Please use code - don't manually do this.
+
+ Using the dog breed API
+
+ Docs: https://dog.ceo/dog-api/documentation/breed
+
+ API URL: https://dog.ceo/api/breed/{breed}/images/random
+
+ */
